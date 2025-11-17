5df2b24dcfd605fb933f79b1bce19438d62560e1
Luca Quaresima <luca.quaresima@studenti.univr.it>
2025-11-17 18:30:29 +0100

    Admin: add controller skeleton and FXML fields

--- src/main/java/application/controllers/AdminAreaController.java
+++ src/main/java/application/controllers/AdminAreaController.java
@@ -1,6 +1,9 @@
 package application.controllers;
 
-import application.logic.SharedResources;
+import application.logic.*;
+import application.repositories.PetriNetRepository;
+import application.repositories.UserRepository;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,82 +11,68 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
+/**
+ * Controller for AdminArea.fxml
+ * Implements Use Cases 6.1.1 and 6.1.2 (Manage Nets and Computations)
+ */
 public class AdminAreaController implements Initializable {
 
     private SharedResources sharedResources;
+    private ProcessService processService;
+    private PetriNetRepository petriNetRepository;
+    private UserRepository userRepository;
+
+    private User currentUser;
     private Stage stage;
 
-    public void setSharedResources(SharedResources sharedResources) {
-        this.sharedResources = sharedResources;
-    }
-
     @FXML
-    private void goToExploreNets(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/ExploreNets.fxml"));
-        Parent root = loader.load();
-
-        ExploreNetsController controller = loader.getController();
-        controller.setSharedResources(sharedResources);
-        controller.setStage((Stage) ((Node) event.getSource()).getScene().getWindow());
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
-
-    public void goToMainView(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
-        Parent mainPage = loader.load();
-
-        MainViewController controller = loader.getController();
-        controller.setSharedResources(sharedResources);
-        controller.setStage((Stage) ((Node) event.getSource()).getScene().getWindow());
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(mainPage));
-        stage.show();
-    }
-
-    public void goToHelp(ActionEvent event) throws IOException {
-        // TODO
-    }
-
-    public void goToNetCreation(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/NetCreation.fxml"));
-        Parent root = loader.load();
-
-        NetCreationController controller = loader.getController();
-        controller.setSharedResources(sharedResources);
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
-
-    public void logOut(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/LoginView.fxml"));
-        Parent root = loader.load();
-
-        LoginViewController controller = loader.getController();
-        controller.setSharedResources(sharedResources);
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
+    private Label errorLabel;
+    @FXML
+    private ListView<String> myNetsListView;
+    @FXML
+    private ListView<Computation> computationsListView;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Nessuna inizializzazione specifica
+    public void initialize(URL location, ResourceBundle resources) {
+        this.sharedResources=SharedResources.getInstance();
+        this.processService=sharedResources.getProcessService();
+        this.petriNetRepository=sharedResources.getPetriNetRepository();
+        this.userRepository=sharedResources.getUserRepository();
+
+        if (errorLabel != null) {
+            errorLabel.setText("");
+        }
     }
 
-    public void setStage(Stage stage) {
-        this.stage = stage;
+    /**
+     * Ingected by MainViewController
+     * @param stage
+     */
+    public void setStage(Stage stage) {this.stage=stage;}
+
+
+    /**
+     * Displays error message
+     * @param message to show
+     */
+    private void showError(String message) {
+        if(errorLabel!=null){
+            errorLabel.setText(message);
+        }else{
+            System.err.println("errorLabel is null in AdminAreaController");
+        }
     }
+
+
 }
