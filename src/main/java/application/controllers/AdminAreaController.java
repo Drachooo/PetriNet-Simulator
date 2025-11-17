6310666f8abcfa25808cb618cc241906515642f3
Luca Quaresima <luca.quaresima@studenti.univr.it>
2025-11-17 18:58:38 +0100

    Admin: add methods to display text in ListViews

--- src/main/java/application/controllers/AdminAreaController.java
+++ src/main/java/application/controllers/AdminAreaController.java
@@ -39,7 +39,7 @@ public class AdminAreaController implements Initializable {
     @FXML
     private Label errorLabel;
     @FXML
-    private ListView<String> myNetsListView;
+    private ListView<PetriNet> myNetsListView;
     @FXML
     private ListView<Computation> computationsListView;
 
@@ -73,6 +73,75 @@ public class AdminAreaController implements Initializable {
             System.err.println("errorLabel is null in AdminAreaController");
         }
     }
+
+    /**
+     * Injected by MainViewController
+     * @param currentUser user
+     */
+    public void setCurrentUser(User currentUser) {
+        this.currentUser=currentUser;
+
+        if(currentUser==null || !currentUser.isAdmin()){
+            showError("User is not admin");
+        }
+        setupListViewFormatters();
+        refreshData();
+    }
+
+    /**
+     * Configures the ListViews to display text.
+     */
+    private void setupListViewFormatters() {
+        // Formatter for the Petri Nets list
+        myNetsListView.setCellFactory(lv -> new ListCell<PetriNet>() {
+            @Override
+            protected void updateItem(PetriNet net, boolean empty) {
+                super.updateItem(net, empty);
+                setText(empty ? null : net.getName() + " (ID: " + net.getId() + ")");
+            }
+        });
+
+        // Formatter for the Computations list
+        computationsListView.setCellFactory(lv -> new ListCell<Computation>() {
+            @Override
+            protected void updateItem(Computation comp, boolean empty) {
+                super.updateItem(comp, empty);
+                if (empty || comp == null) {
+                    setText(null);
+                } else {
+                    PetriNet net = petriNetRepository.getPetriNets().get(comp.getPetriNetId());
+                    User user = userRepository.getUserById(comp.getUserId());
+
+                    if (net != null && user != null) {
+                        setText(net.getName() + " - Run by: " + user.getEmail() + " - Status: " + comp.getStatus());
+                    } else {
+                        setText("Loading data...");
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Filters all the nets to find the admin's ones.
+     * Calls getComptationsForAdmin to obtain computations on the nets
+     * Uses setItems to give these two lists to the ListView(s) that will show them using setCellFactory
+     */
+    private void refreshData() {
+        errorLabel.setText("");
+
+        // 1. Populate "My Created Nets" (Use Case 6.1.1)
+        List<PetriNet> myNets = petriNetRepository.getPetriNets().values().stream()
+                .filter(net -> net.getAdminId().equals(currentUser.getId()))
+                .collect(Collectors.toList());
+
+        myNetsListView.setItems(FXCollections.observableArrayList(myNets));
+
+        // 2. Populate "Computations on My Nets" (Use Case 6.1.2)
+        List<Computation> adminComputations = processService.getComputationsForAdmin(currentUser.getId());
+        computationsListView.setItems(FXCollections.observableArrayList(adminComputations));
+    }
+
 
 
 }
